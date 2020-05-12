cf129575117fdb13d1b6b76bf9ee33c61924b0e2
Александр Тихонович <rulers.oftheinfinite@mail.ru>
2020-05-12 20:00:48 +0300

    Updates

---
+++ precache-manifest.c0f546b893f9134335a60a23f87540d5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a647b6ddc308f94dd84377766c53fea2",
+    "url": "/chef/index.html"
+  },
+  {
+    "revision": "0df171e161ce4414cb56",
+    "url": "/chef/static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "db386827d0170f923399",
+    "url": "/chef/static/css/main.e34dd6f3.chunk.css"
+  },
+  {
+    "revision": "0df171e161ce4414cb56",
+    "url": "/chef/static/js/2.c7fc731c.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/chef/static/js/2.c7fc731c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db386827d0170f923399",
+    "url": "/chef/static/js/main.c9ce97b4.chunk.js"
+  },
+  {
+    "revision": "ae323d9bb2e6acdbc351",
+    "url": "/chef/static/js/runtime-main.9d25b0d1.js"
+  }
+]);
